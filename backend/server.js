71f85940c74a1f919593fe10cb48f161b22146d9
Shadwal Chauhan <shadwalchauhan@gmail.com>
2026-08-02 14:54:59 +0530

    Prepared backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const Booking = require('./models/Booking');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    credentials: true
+}));
 app.use(express.json());
 
 // Database Connection
